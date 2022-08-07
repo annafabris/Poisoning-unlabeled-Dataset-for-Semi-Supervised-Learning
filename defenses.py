cdb4f192c8fdd01f160a59f68869007c2c5ddf20
annafabris <xxanninaxx4@gmail.com>
2022-08-07 21:27:57 +0200

    Code improvement

--- defenses.py
+++ defenses.py
@@ -1,27 +1,32 @@
 from keras.datasets import mnist
-from keras.datasets import mnist
 from sklearn.cluster import AgglomerativeClustering
 import numpy as np
 import csv
 import os
 from collections import Counter
 
-#get the dataset
-inp_size = 28 * 28 # size of mnist dataset 
+# get the dataset
+inp_size = 28 * 28  # size of mnist dataset
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-x_train = x_train.reshape(60000, inp_size).astype('float32')/255.0
-y_train =[1 for i in x_train]
+x_train = x_train.reshape(60000, inp_size).astype("float32") / 255.0
+y_train = [1 for _ in x_train]
 
 # choose which pre donwloaded data to include
-interpolation = 'image'
-if(interpolation == 'image'):      # 1800 images of 4-9 image space interpolation
-    path = os.path.dirname(os.path.abspath(__file__)) + '/poisoned_data/image_space_1800.csv'
-elif(interpolation == 'latent'):    # 1800 images of 4-9 latent space interpolation
-    path = os.path.dirname(os.path.abspath(__file__)) + '/poisoned_data/latent_space_1800.csv'
+interpolation = "image"
+if interpolation == "image":  # 1800 images of 4-9 image space interpolation
+    path = (
+        os.path.dirname(os.path.abspath(__file__))
+        + "/poisoned_data/image_space_1800.csv"
+    )
+elif interpolation == "latent":  # 1800 images of 4-9 latent space interpolation
+    path = (
+        os.path.dirname(os.path.abspath(__file__))
+        + "/poisoned_data/latent_space_1800.csv"
+    )
 
 # download poisoned data and add it
-with open(path, newline='') as f:
+with open(path, newline="") as f:
     reader = csv.reader(f)
     poison_dataset = np.array(list(reader))
 
@@ -31,7 +36,7 @@ y_poison_dataset = [0 if i < len(x_train) else 1 for i in range(len(x_poison_dat
 
 # perform the AgglomerativeClustering algorithm to identify poisoned samples
 def agglomerative_clustering_defense():
-    clustering = AgglomerativeClustering(n_clusters = 10).fit(x_poison_dataset)
+    clustering = AgglomerativeClustering(n_clusters=10).fit(x_poison_dataset)
 
     # check how many elements have been identified as poisoned
     poisoned_cluster = Counter(clustering.labels_).most_common(1)[0][0]
@@ -39,14 +44,31 @@ def agglomerative_clustering_defense():
     clean_count = 0
     poisoned_count = 0
     for i in range(len(clustering.labels_) - 1, 0, -1):
-        if(clustering.labels_[i] == poisoned_cluster):
-            if(y_poison_dataset[i] == 0):
+        if clustering.labels_[i] == poisoned_cluster:
+            if y_poison_dataset[i] == 0:
                 clean_count += 1
             else:
                 poisoned_count += 1
     print("Agglomerative Clustering Defense")
-    print("Percentage of not-poisoned data identified as poisoned: " + str(clean_count * 100 // (len(y_poison_dataset) - sum(y_poison_dataset))) + "% (" + str(clean_count) + "/" + str(len(y_poison_dataset) - sum(y_poison_dataset)) + ")")
-    print("Percentage of poisoned data identified as poisoned: " + str(poisoned_count * 100 // sum(y_poison_dataset)) + "% (" + str(poisoned_count) + "/" + str(sum(y_poison_dataset)) + ")")
+    print(
+        "Percentage of not-poisoned data identified as poisoned: "
+        + str(clean_count * 100 // (len(y_poison_dataset) - sum(y_poison_dataset)))
+        + "% ("
+        + str(clean_count)
+        + "/"
+        + str(len(y_poison_dataset) - sum(y_poison_dataset))
+        + ")"
+    )
+    print(
+        "Percentage of poisoned data identified as poisoned: "
+        + str(poisoned_count * 100 // sum(y_poison_dataset))
+        + "% ("
+        + str(poisoned_count)
+        + "/"
+        + str(sum(y_poison_dataset))
+        + ")"
+    )
+
 
 # Monitoring the training dynamics defense to identify poisoned samples
 def monitoring_training_dynamics_defense(predictons, number_of_poisoned_samples):
@@ -68,15 +90,27 @@ def monitoring_training_dynamics_defense(predictons, number_of_poisoned_samples)
     influence = np.empty((number_of_samples, number_of_samples))
     for i in range(0, number_of_samples):
         for j in range(0, number_of_samples):
-            influence[i][j] = np.sum(np.power((np.array(prediction_changing[i][:len(prediction_changing[0]) - 1]) - np.array(prediction_changing[j][1:len(prediction_changing[0])])), 2))
-    
+            influence[i][j] = np.sum(
+                np.power(
+                    (
+                        np.array(
+                            prediction_changing[i][: len(prediction_changing[0]) - 1]
+                        )
+                        - np.array(
+                            prediction_changing[j][1 : len(prediction_changing[0])]
+                        )
+                    ),
+                    2,
+                )
+            )
+
     # calculate the average influence of the k nearest neighbors
     # avg influence(u) = 1/k ∑v∈U Influence(u, v)·1[closek(u, v)]
     avg_influence = np.empty(number_of_samples)
     k = 10
     for i in range(number_of_samples):
         close_k = influence[i]
-        close_k = sorted(close_k, reverse = True)        
+        close_k = sorted(close_k, reverse=True)
         avg_influence[i] = sum(close_k[:k]) / k
 
     # check how many elements have been identified as poisoned
@@ -84,11 +118,31 @@ def monitoring_training_dynamics_defense(predictons, number_of_poisoned_samples)
     poisoned_identified = 0
     threshold = np.percentile(avg_influence, 85)
     for i in range(number_of_samples):
-        if(avg_influence[i] >= threshold):
-            if(i <= number_of_samples - number_of_poisoned_samples):
+        if avg_influence[i] >= threshold:
+            if i <= number_of_samples - number_of_poisoned_samples:
                 not_poisoned_identified += 1
             else:
                 poisoned_identified += 1
     print("Monitoring Training Dynamics Defense")
-    print("Percentage of not-poisoned data identified as poisoned: " + str(100 * not_poisoned_identified // (number_of_samples- number_of_poisoned_samples))+ "% (" + str(not_poisoned_identified) + "/" + str(number_of_samples - number_of_poisoned_samples) + ")")
-    print("Percentage of poisoned data identified as poisoned: " + str(100 * poisoned_identified // number_of_poisoned_samples) + "% (" + str(poisoned_identified) + "/" + str(number_of_poisoned_samples) + ")")
+    print(
+        "Percentage of not-poisoned data identified as poisoned: "
+        + str(
+            100
+            * not_poisoned_identified
+            // (number_of_samples - number_of_poisoned_samples)
+        )
+        + "% ("
+        + str(not_poisoned_identified)
+        + "/"
+        + str(number_of_samples - number_of_poisoned_samples)
+        + ")"
+    )
+    print(
+        "Percentage of poisoned data identified as poisoned: "
+        + str(100 * poisoned_identified // number_of_poisoned_samples)
+        + "% ("
+        + str(poisoned_identified)
+        + "/"
+        + str(number_of_poisoned_samples)
+        + ")"
+    )
